c73be9af18a2fda6e7b06cc95e4a87d770d4cb0f
Uok04 <usaya382@gmail.com>
2024-03-01 11:04:53 +0900

    ユーザー管理機能についてのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :password, presence: true, length: { minimum: 6 }
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :position, presence: true
   validates :affiliation, presence: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,9 @@
 FactoryBot.define do
   factory :user do
-    nickname { "" }
+    nickname { "日々積　助久" }
     email { Faker::Internet.email }
     encrypted_password { "Password1" } # 6文字以上で半角英数字混合
-    last_name { "山田" } # 全角文字
-    first_name { "太郎" } # 全角文字
-    last_name_kana { "ヤマダ" } # 全角カタカナ
-    first_name_kana { "タロウ" } # 全角カタカナ
-    birthday { Date.today }
+    position { "部長" }
+    affiliation { "Webエンジニア" }
   end
 end
