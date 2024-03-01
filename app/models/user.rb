ccfbd3c28fa1f0fb96afa51b7c02e8ced1bb3f1e
Uok04 <usaya382@gmail.com>
2024-03-01 09:00:28 +0900

    ユーザー管理機能、エラーハンドリングの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :position, presence: true
+  validates :affiliation, presence: true
+
 end
 
