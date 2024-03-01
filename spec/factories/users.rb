cac60905362813e36f85b27ecc73ec48489d4731
Uok04 <usaya382@gmail.com>
2024-03-01 22:00:09 +0900

    ユーザー管理機能モデル単体テスト実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname { "日々積　助久" }
     email { Faker::Internet.email }
-    encrypted_password { "Password1" } # 6文字以上で半角英数字混合
+    password {"Abc123"}
+    password_confirmation { password }
     position { "部長" }
     affiliation { "Webエンジニア" }
   end
