25cfdfd4022bd2882b429e8362e16e73d35c6c40
Uok04 <usaya382@gmail.com>
2024-03-17 19:51:30 +0900

    タスク投稿機能における表示、保存の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,22 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to root_path
+  end
+  
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name, :description, :category_id, :deadline).merge(user_id: current_user.id)
+  end
+
 end
