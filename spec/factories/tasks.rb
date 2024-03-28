27a9bc9d1c97a94005cb5e506b9006358a529f88
Uok04 <usaya382@gmail.com>
2024-03-28 17:02:28 +0900

    タスク投稿機能におけるモデル単体テストの実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :task do
-    
+    association :user
+    task_name { "タスク" }
+    description { "割引" }
+    category_id { "2" }
+    deadline { Date.today }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @task = FactoryBot.build(:task)
+  end
+
+  describe 'タスク投稿' do
+    context 'タスク投稿できる場合' do
+      it 'task_nameとdescriptionとdeadlineとcategory_idが存在すれば登録できる' do
+        expect(@task).to be_valid
+      end
+    end
+
+    context 'タスク投稿できない場合' do
+      it 'task_nameが空では登録できない' do
+        @task.task_name = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include("Task name can't be blank")
+      end
+
+      it 'descriptionが空では登録できない' do
+        @task.description = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'deadlineが空では登録できない' do
+        @task.deadline = ''
+        @task.valid?
+        expect(@task.errors.full_messages).to include("Deadline can't be blank")
+      end
+
+      it 'category_idが空では登録できない' do
+        @task.category_id = '1'
+        @task.valid?
+        expect(@task.errors.full_messages).to include("Category can't be blank")
+      end
+    end
+  end
 end
